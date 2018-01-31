2a945eafec76157ad7c174df648c7372a43e7304
spartahawk <spartahawk.git@gmail.com>
2018-01-31 08:53:09 -1000

    All computers assigned number 16, for debugging purposes
    Assignment to computer16 isn't actually assigning computer16. All computers getting the assignment for
    debugging.

--- src/player_computer.c
+++ src/player_computer.c
@@ -1653,8 +1653,13 @@ void setup_computer_players2()
         int skirmish_AI_type = rand() % (maxSkirmishAI + 1 - minSkirmishAI) + minSkirmishAI;
 		//TODO: Make log message only for non-human players
         JUSTMSG("Player %d given computer model %d.", i, skirmish_AI_type);
-        JUSTMSG("* \(If player %d is human, this only applies if they leave the game.\)", i);
-        setup_a_computer_player(i, skirmish_AI_type);
+        if (i == PLAYERS_COUNT - 1)
+        {
+            JUSTMSG("*For any human players above, this only applies if they leave the game.");
+        }
+        // For testing purposes only: (all computer players assigned 16).
+        // Change second parameter back to skirmish_AI_type afterward
+        setup_a_computer_player(i, 16);
 #endif
       }
     }
