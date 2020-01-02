607e937448fc691c2aadf28eb8b5d7afe96ff18b
Loobinex <Loobinex@users.noreply.github.com>
2020-01-02 15:18:03 +0100

    Partial commit

--- src/frontend.cpp
+++ src/frontend.cpp
@@ -2578,6 +2578,9 @@ void frontend_shutdown_state(FrontendMenuState pstate)
         turn_off_menu(GMnu_FELEVEL_SELECT);
         frontend_level_list_unload();
         break;
+    case FeSt_MAPPACK_SELECT:
+        turn_off_menu(FeSt_MAPPACK_SELECT);
+        break;
     case FeSt_CAMPAIGN_SELECT:
         turn_off_menu(GMnu_FECAMPAIGN_SELECT);
         break;
@@ -2722,6 +2725,10 @@ FrontendMenuState frontend_setup_state(FrontendMenuState nstate)
     case FeSt_LEVEL_SELECT:
         turn_on_menu(GMnu_FELEVEL_SELECT);
         frontend_level_list_load();
+        set_pointer_graphic_menu();
+        break;
+    case FeSt_MAPPACK_SELECT:
+        turn_on_menu(GMnu_MAPPACK_SELECT);
         set_pointer_graphic_menu();
         break;
     case FeSt_CAMPAIGN_SELECT:
@@ -3196,6 +3203,7 @@ short frontend_draw(void)
     case FeSt_UNKNOWN20:
     case FeSt_FEOPTIONS:
     case FeSt_LEVEL_SELECT:
+    case FeSt_MAPPACK_SELECT:
     case FeSt_CAMPAIGN_SELECT:
         frontend_copy_background();
         draw_gui();
