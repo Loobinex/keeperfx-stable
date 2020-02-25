777c786f3a2d1b0a753562ee57b542bc23f88f9c
Loobinex <Loobinex@users.noreply.github.com>
2020-02-25 02:11:12 +0100

    Replaced instance/spell group with Lizard

--- src/creature_states_combt.c
+++ src/creature_states_combt.c
@@ -78,7 +78,6 @@ const CombatState combat_door_state[] = {
 const struct CombatWeapon offensive_weapon[] = {
     {CrInst_FREEZE,                 156, LONG_MAX},
     {CrInst_FEAR,                   156, LONG_MAX},
-    {CrInst_CAST_SPELL_GROUP,       156, LONG_MAX},
     {CrInst_CAST_SPELL_DISEASE,     156, LONG_MAX},
     {CrInst_CAST_SPELL_CHICKEN,     156, LONG_MAX},
     {CrInst_CAST_SPELL_TIME_BOMB,   768, LONG_MAX},
