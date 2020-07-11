354a58d3836beba4c8f6aaf32615276b97ab4fef
Loobinex <Loobinex@users.noreply.github.com>
2020-07-11 20:09:01 +0200

    Fixed header
    which was done in previous commit in branch

--- src/main.cpp
+++ src/main.cpp
@@ -3741,7 +3741,7 @@ int can_thing_be_queried(struct Thing *thing, long a2)
   return _DK_can_thing_be_queried(thing, a2);
 }
 
-void tag_cursor_blocks_sell_area(unsigned char a1, long a2, long a3, long a4)
+void tag_cursor_blocks_sell_area(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, long a4)
 {
     SYNCDBG(7,"Starting");
     // _DK_tag_cursor_blocks_sell_area(plyr_idx, stl_x, stl_y, a4);
