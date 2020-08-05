1d551adcd9e3b38ff66258534022783cc304bbf1
eddebaby <ed_j_kearney@yahoo.co.uk>
2020-08-05 20:08:53 +0100

    Add strings for new map categories
    Add string for "The Deeper Dungeons", "Dungeon Keeper maps", and "KeeperFX maps".
    
    Usable by the frontend menu

--- src/config_strings.h
+++ src/config_strings.h
@@ -405,6 +405,9 @@ enum GUIStrings {
     GUIStr_CreatureHealthDesc = STRINGS_MAX+965,
     GUIStr_CreatureMaxHealthDesc = STRINGS_MAX+966,
     GUIStr_ToggleMessage = STRINGS_MAX+967,
+    GUIStr_MnuDeeperDungeons = STRINGS_MAX+968,
+    GUIStr_MnuDungeonKeeperLevels = STRINGS_MAX+969,
+    GUIStr_MnuKeeperFXLevels = STRINGS_MAX+970,
 };
 
 enum CampaignStrings {
