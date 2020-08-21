528526c043b72e31ae325b461644a67e9bff5393
Loobinex <Loobinex@users.noreply.github.com>
2020-08-21 23:35:18 +0200

    Renamed FITPP to PIXELPERFECT

--- src/config.c
+++ src/config.c
@@ -132,15 +132,15 @@ const struct NamedCommand logicval_type[] = {
   };
 
   const struct NamedCommand vidscale_type[] = {
-  {"OFF",     256}, // = 0x100 = No scaling of Smacker Video
-  {"FIT",      16}, // = 0x10 = SMK_FullscreenFit - fit to fullscreen, using letterbox and pillarbox as necessary
-  {"ON",       16}, // TEMPORARY = duplicates FIT
-  {"STRETCH",  32}, // = 0x20 = SMK_FullscreenStretch  - stretch to fullscreen - ignores aspect ratio difference between source and destination
-  {"CROP",     64}, // = 0x40 = SMK_FullscreenCrop - fill fullscreen and crop - no letterbox or pillarbox
-  {"4BY3",     48}, // = 0x10 & 0x20 = [Aspect Ratio correction mode] - stretch 320x200 to 4:3 (i.e. increase height by 1.2)
-  {"FITPP",    80}, // = 0x10 & 0x40 = integer multiple scale only (FIT)
-  {"4BY3PP",  112}, // = 0x10 & 0x20 & 0x40 = integer multiple scale only (4BY3)
-  {NULL,        0},
+  {"OFF",          256}, // = 0x100 = No scaling of Smacker Video
+  {"FIT",           16}, // = 0x10 = SMK_FullscreenFit - fit to fullscreen, using letterbox and pillarbox as necessary
+  {"ON",            16}, // Duplicate of FIT, for legacy reasons
+  {"STRETCH",       32}, // = 0x20 = SMK_FullscreenStretch  - stretch to fullscreen - ignores aspect ratio difference between source and destination
+  {"CROP",          64}, // = 0x40 = SMK_FullscreenCrop - fill fullscreen and crop - no letterbox or pillarbox
+  {"4BY3",          48}, // = 0x10 & 0x20 = [Aspect Ratio correction mode] - stretch 320x200 to 4:3 (i.e. increase height by 1.2)
+  {"PIXELPERFECT",  80}, // = 0x10 & 0x40 = integer multiple scale only (FIT)
+  {"4BY3PP",       112}, // = 0x10 & 0x20 & 0x40 = integer multiple scale only (4BY3)
+  {NULL,             0},
   };
 unsigned int vid_scale_flags = 0;
 
